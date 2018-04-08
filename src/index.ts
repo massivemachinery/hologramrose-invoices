90900351859cd766d1e0f8b8c44fddcc8db99dc2
Mikael Lirbank <mikael@airlab.io>
2018-04-07 17:30:06 -0700

    Ensure https

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import ensureRootUrl from 'ensure-root-url';
 
 const port = process.env.PORT || '5000';
+const rootUrl = process.env.ROOT_URL || 'http://localhost:5000';
 
 async function server() {
   const app = express();
+
+  app.use(ensureRootUrl(rootUrl));
 
   app.use('/*', (req, res) => {
     res.send('OK');
