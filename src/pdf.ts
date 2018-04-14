09e0b141dd0fec4993c3e19e32687365b04c4282
Mikael Lirbank <mikael@airlab.io>
2018-04-13 20:34:36 -0700

    Puppeteer launch config

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,7 +1,9 @@
 import puppeteer from 'puppeteer';
 
 export default async function htmlToPdf(html: string) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.setContent(html);
   await page.waitFor(1000);
